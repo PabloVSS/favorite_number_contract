51a9112d3e153d505e6e82ad3688d19acea774e1
PabloVSS <svinipb@gmail.com>
2024-12-05 09:27:12 -0300

    criação

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24; // solidity versions
+
+contract SimpleStorage{
+
+    // Basic Type: boolean = true or false, uint = int positive, int = int positive or negative, adress = adress, byte
+
+    //favoreiteNumber gets initialized to 0 if no value is given
+    
+    uint256 public myfavoriteNumber;
+
+    
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }    
+
+    //Dinamic array
+    Person [] public listOfPeople;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        myfavoriteNumber = _favoriteNumber;
+    }
+
+    //view: return some value
+
+    function retrive() public view returns (uint256) {
+        return myfavoriteNumber;
+     }
+     
+     //calldata, memory, storage
+     function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+     }
+}
+
+
